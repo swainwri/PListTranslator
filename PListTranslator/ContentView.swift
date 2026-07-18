9f5f57135a889f865ae8c2688b9d44f8baa31e05
swainwri <wainwright_steve61@yahoo.com>
2026-07-19 02:46:30 +0700

    Initial Commit

---
+++ PListTranslator/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PListTranslator
+//
+//  Created by Steve Wainwright on 19/07/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PListTranslator/PListTranslatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  PListTranslatorApp.swift
+//  PListTranslator
+//
+//  Created by Steve Wainwright on 19/07/2026.
+//
+
+import SwiftUI
+
+@main
+struct PListTranslatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
